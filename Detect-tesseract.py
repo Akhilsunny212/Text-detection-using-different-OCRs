a2269d81ff7916abf91f32b64d16ba748da51d80
Akhilsunny212 <78971481+Akhilsunny212@users.noreply.github.com>
2025-01-03 16:41:59 +0530

    Update Detect-tesseract.py

--- Detect-tesseract.py
+++ Detect-tesseract.py
@@ -5,6 +5,6 @@ pytesseract.pytesseract.tesseract_cmd = "Path to your Tesseract location in loca
 # Load your image
 image = cv2.imread("Path to your image")
 
-text = pytesseract.image_to_string(image, config='--psm 11')
+extracted_text = pytesseract.image_to_string(image, config='--psm 11')
 # provide psm values based on the requirement
-print(text)
+print(extracted_text)
