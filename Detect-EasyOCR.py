f67ca858a4741b8e92d7f720a3162c5f34f8bf0b
Akhilsunny212 <78971481+Akhilsunny212@users.noreply.github.com>
2025-01-20 19:28:16 +0530

    Update Detect-EasyOCR.py

--- Detect-EasyOCR.py
+++ Detect-EasyOCR.py
@@ -3,6 +3,7 @@ import easyocr
 
 image = cv2.imread("Path to your image")
 
+
 reader = easyocr.Reader(['en'], gpu=False)
 text_detections = reader.readtext(image)
 text1=text_detections.split("\n")
